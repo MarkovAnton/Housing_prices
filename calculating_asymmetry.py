84ce93236b989573a2054913ad2d004f5f2f539b
MarkovAnton <markov_anton2001@mail.ru>
2021-06-25 10:22:12 +0300

    add calculating_asymmetry.py

---
+++ calculating_asymmetry.py
@@ -0,0 +1,11 @@
+import numpy as np
+from scipy.stats import skew
+
+
+def calc_asymmetry(train, full_df, num_features):
+    skewed_feats = train[num_features].apply(lambda x: skew(x.dropna()))
+    skewed_feats = skewed_feats[skewed_feats > 0.75]
+    skewed_feats = skewed_feats.index
+    full_df[skewed_feats] = np.log1p(full_df[skewed_feats])
+
+    return full_df
